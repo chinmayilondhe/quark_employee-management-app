c9093e1b2d722042665a6da81397d507ab11b8a7
chinmayilondhe <chinmayilondhe73@gmail.com>
2022-10-10 23:34:56 +0530

    solved merge conflicts

--- lib/helper/piechart_employee.dart
+++ lib/helper/piechart_employee.dart
@@ -1,15 +1,8 @@
-
-import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-
-import '../data/pie_data.dart';
-
 import 'package:flipr/helper/shared_pref_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import '../model/response_body.dart';
 import 'api_model.dart';
-
 
 class PieChartGraph extends StatefulWidget {
   const PieChartGraph({Key? key}) : super(key: key);
@@ -20,18 +13,20 @@ class PieChartGraph extends StatefulWidget {
 
 class _PieChartGraphState extends State<PieChartGraph> {
   // PieData obj = PieData();
-   List<Data> _chartData=[];
+  List<Data> _chartData = [];
   Api apiCall = Api();
   SharedPrefManager prefs = SharedPrefManager();
   late TooltipBehavior _tooltipBehavior;
-  List<Data> chartDataGraph=[];
-  String temp="",empId="";
+  List<Data> chartDataGraph = [];
+  String temp = "", empId = "";
+
   getDataGraph() async {
     chartDataGraph = await getData();
     setState(() {
-        _chartData = chartDataGraph;
+      _chartData = chartDataGraph;
     });
   }
+
   _loadEmpId() async {
     temp = await prefs.getEmpID();
 
@@ -58,13 +53,20 @@ class _PieChartGraphState extends State<PieChartGraph> {
         width: 350,
         child: SfCircularChart(
           backgroundColor: Colors.lightGreen.shade100,
-            // margin: EdgeInsets.all(15),
-          title: ChartTitle(text: 'Task Distribution of Employee',
-              textStyle: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.bold)),
-          legend:
-              Legend(isVisible: true, overflowMode: LegendItemOverflowMode.wrap,
-                  position: LegendPosition.top,toggleSeriesVisibility: true,
-                  offset: Offset(10, 2),height: '30%'),
+          // margin: EdgeInsets.all(15),
+          title: ChartTitle(
+              text: 'Task Distribution of Employee',
+              textStyle: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold)),
+          legend: Legend(
+              isVisible: true,
+              overflowMode: LegendItemOverflowMode.wrap,
+              position: LegendPosition.top,
+              toggleSeriesVisibility: true,
+              offset: Offset(10, 2),
+              height: '30%'),
           tooltipBehavior: _tooltipBehavior,
           series: <CircularSeries>[
             PieSeries<Data, String>(
@@ -72,8 +74,9 @@ class _PieChartGraphState extends State<PieChartGraph> {
               dataSource: _chartData,
               xValueMapper: (Data data, _) => data.task,
               yValueMapper: (Data data, _) => data.val,
-              dataLabelSettings: DataLabelSettings(isVisible: true,
-              textStyle: TextStyle(fontSize: 15,color: Colors.white)),
+              dataLabelSettings: DataLabelSettings(
+                  isVisible: true,
+                  textStyle: TextStyle(fontSize: 15, color: Colors.white)),
               enableTooltip: true,
             )
           ],
@@ -81,8 +84,8 @@ class _PieChartGraphState extends State<PieChartGraph> {
       ),
     ));
   }
-   getData() async {
 
+  getData() async {
     var break_val, meet_val, work_val;
     empId = await prefs.getEmpID();
     ResponseBody responseBody = await apiCall.getPieChart(empId);
@@ -97,8 +100,10 @@ class _PieChartGraphState extends State<PieChartGraph> {
     return data;
   }
 }
+
 class Data {
   final String task;
   final double val;
+
   Data(this.task, this.val);
 }

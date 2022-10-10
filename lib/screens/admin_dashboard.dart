7850341417b40b8541a43908b9afcdf3014dfac5
Chaitanya1211 <84672722+Chaitanya1211@users.noreply.github.com>
2022-10-10 19:44:10 +0530

    Chaitanya (#6)
    * admin dashboard half done
    
    * admin dashboard done

--- lib/screens/admin_dashboard.dart
+++ lib/screens/admin_dashboard.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flipr/helper/shared_pref_manager.dart';
 import 'package:flipr/screens/add_employee.dart';
+import 'package:flipr/screens/employee_dashboard.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import '../helper/piechart.dart';
 
 class Admin extends StatefulWidget {
@@ -12,11 +15,28 @@ class Admin extends StatefulWidget {
 }
 
 class _AdminState extends State<Admin> {
-  SharedPrefManager sharedData = SharedPrefManager();
-  var emp_id;
-  void getRoleStatus() async {
-   emp_id = await sharedData.getEmpID();
+  getEmployee() async {
+    var response = await http.get(
+        Uri.https('omkar3602-flipr-backend.herokuapp.com', 'employee/list'));
+
+    var jsonData = jsonDecode(response.body);
+    List<EmployeeData> employee = [];
+    for (var u in jsonData) {
+      EmployeeData e = EmployeeData(u['name'], u['department'], u['_id']);
+      if (u['role'] == "emp") {
+        employee.add(e);
+      }
+    }
+
+    print(employee.length);
+    return employee;
   }
+
+  // SharedPrefManager sharedData = SharedPrefManager();
+  // var emp_id;
+  // void getRoleStatus() async {
+  //  emp_id = await sharedData.getEmpID();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -24,8 +44,55 @@ class _AdminState extends State<Admin> {
       appBar: AppBar(
         title: Text('Admin'),
       ),
+      // body: Container(
+      //   child: PieChartGraph(),
+      // ),
       body: Container(
-        child: PieChartGraph(),
+        child: Card(
+            child: FutureBuilder(
+                future: getEmployee(),
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (snapshot.data == null) {
+                    return Container(
+                      child: Center(
+                        child: Text("Loading"),
+                      ),
+                    );
+                  } else {
+                    // var l= snapshot.data?.length;
+                    // var length;
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, i) {
+                        return Card(
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(snapshot.data[i].name),
+                                      Text(snapshot.data[i].department)
+                                    ],
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        print(snapshot.data[i].id);
+                                      },
+                                      child: Text("See Details"))
+                                ]),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                })),
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
@@ -36,4 +103,10 @@ class _AdminState extends State<Admin> {
           label: Text("Add New Employee")),
     );
   }
+}
+
+class EmployeeData {
+  final String name, department, id;
+
+  EmployeeData(this.name, this.department, this.id);
 }
